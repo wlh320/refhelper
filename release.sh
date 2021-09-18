14f02aa43df18977a00289dcfd0574d810d6e332
wlh <wanglinghao18z@ict.ac.cn>
2021-09-18 13:48:11 +0800

    reduce executable size 2

--- release.sh
+++ release.sh
@@ -3,11 +3,11 @@ cargo build --release --target x86_64-unknown-linux-musl
 cargo build --release --target x86_64-pc-windows-gnu
 cd target/x86_64-unknown-linux-musl/release
 strip refhelper
-upx -9 refhelper
+upx --best --lzma refhelper
 tar -czvf refhelper_x86_64-unknown-linux-musl.tar.gz refhelper
 cp refhelper_x86_64-unknown-linux-musl.tar.gz ../../
 cd ../../x86_64-pc-windows-gnu/release
 strip refhelper.exe
-upx -9 --force refhelper.exe
+upx --best --lzma --force refhelper.exe
 zip refhelper_x86_64-pc-windows-gnu.zip refhelper.exe
 cp refhelper_x86_64-pc-windows-gnu.zip ../../
